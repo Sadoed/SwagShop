d95d2154cde4b88607e3a0033847e2b389cf41cd
Sadoed <barmoza999@gmail.com>
2019-03-12 23:27:27 +0300

    add: tableview data and delegate

---
+++ Swag_shop/Swag_shop/Controller/CategoriesVC.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Swag_shop
+//
+//  Created by more on 10/03/2019.
+//  Copyright © 2019 Archailect. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController, UITableViewDataSource,
+UITableViewDelegate {
+
+    @IBOutlet weak var categoryTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        categoryTable.dataSource = self
+        categoryTable.delegate = self
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataService.instance.getCategories().count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as? CategoryCell {
+            let category = DataService.instance.getCategories()[indexPath.row]
+            cell.updateViews(category: category)
+            return cell
+        } else {
+            return CategoryCell()
+        }
+    }
+}
+
+
+
---
+++ Swag_shop/Swag_shop/Services/DataService.swift
@@ -0,0 +1,24 @@
+//
+//  DataService.swift
+//  Swag_shop
+//
+//  Created by more on 12/03/2019.
+//  Copyright © 2019 Archailect. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    static let instance = DataService()
+    
+    private let categories = [
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png")
+    ]
+    
+    func getCategories() -> [Category] {
+        return categories
+    }
+}
