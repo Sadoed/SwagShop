2fe1f133ce9ea994648e800123e95c0c6e1f4a68
Sadoed <barmoza999@gmail.com>
2019-03-13 23:32:02 +0300

    add: collection view

---
+++ Swag_shop/Swag_shop/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  Swag_shop
+//
+//  Created by more on 13/03/2019.
+//  Copyright © 2019 Archailect. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+
+}
